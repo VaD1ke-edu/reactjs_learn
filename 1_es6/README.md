3f1f8b5f8eb8344b79b7185063d5e3e4de1d45af
VaD1ke-edu <vladislav.slesarenko@gmail.com>
2019-01-20 11:00:54 +0300

    Refactor first homework

--- 1_es6/README.md
+++ 1_es6/README.md
@@ -1,5 +1,5 @@
 1. loop.js - https://jsbin.com/buvipit/1/edit?js,console
 1. calculateArea.js - https://jsbin.com/zeximov/edit?js,console
 1. employee_classes.js - https://plnkr.co/edit/cp572PJmlmzUd5zE6wsA. Делал в планкере, так как там можно сделать несколько js-файлов
-1. generator.js - https://jsbin.com/moroqe/edit?html,js,console
-1. promises.js - https://jsbin.com/qiwapuc/edit?html,js,console
+1. generator.js - https://jsbin.com/moroqe/edit?js,console
+1. promises.js - https://jsbin.com/qiwapuc/edit?js,console
--- 1_es6/calculateArea.js
+++ 1_es6/calculateArea.js
@@ -1,21 +1,44 @@
-function calculateArea(a, b, c) {
-    let figure, square = 0;
-    a = parseFloat(a);
-    b = parseFloat(b);
-    c = parseFloat(c);
-
-    if (c) {
-        figure = 'triangle';
-        let p = 0.5 * (a + b + c);
-        square = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-    } else {
-        figure = a === b ? 'square' : 'rectangle';
-        square = a * b;
-    }
-    square = Math.round(square * 100) / 100;
-    return {square: square, figure: figure};
+function calculateRectangleSquare(a, b) {
+    return a * b;
 }
 
-console.log(calculateArea(2, 2));    // {square: 4, figure: 'square'}
-console.log(calculateArea(2, 3));    // {square: 6, figure: 'rectangle'}
+function calculateTriangleSquare(a, b, c) {
+    let p = 0.5 * (a + b + c);
+    return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+function round(value) {
+    return Math.round(value * 100) / 100;
+}
+
+function calculateArea(...sides) {
+    for (let i in sides) {
+        sides[i] = parseFloat(sides[i]);
+    }
+
+    const calculatorMap = {
+        1: {
+            getFigure: (sides) => 'square',
+            getSquare: (sides) => calculateRectangleSquare(sides[0], sides[0])
+        },
+        2: {
+            getFigure: (sides) => sides[0] === sides[1] ? 'square' : 'rectangle',
+            getSquare: (sides) => calculateRectangleSquare(sides[0], sides[1])
+        },
+        3: {
+            getFigure: (sides) => 'triangle',
+            getSquare: (sides) => calculateTriangleSquare(sides[0], sides[1], sides[2])
+        }
+    };
+
+    const calculator = calculatorMap[sides.length];
+    return calculator ?
+        {square: round(calculator.getSquare(sides)), figure: calculator.getFigure(sides)} :
+        {square: undefined, figure: 'Undefined figure'};
+}
+
+console.log(calculateArea(2));    // {square: 4, figure: 'square'}
+console.log(calculateArea(2, 2)); // {square: 4, figure: 'square'}
+console.log(calculateArea(2, 3)); // {square: 6, figure: 'rectangle'}
 console.log(calculateArea(2, 3, 4)); // {square: 2.9, figure: 'triangle'}
+console.log(calculateArea(2, 3, 4, 5)); // {square: undefined, figure: 'Undefined figure'}
