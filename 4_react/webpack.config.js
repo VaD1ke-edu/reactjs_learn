5f6e0bf4b632eac7d501d89b074f7d94d3e528b5
VaD1ke-edu <vladislav.slesarenko@gmail.com>
2019-02-01 00:18:36 +0300

    Add 4 homework

---
+++ 4_react/webpack.config.js
@@ -0,0 +1,30 @@
+//подключаем модули
+const path = require('path');
+
+module.exports = {
+
+    entry: {
+        main: path.resolve(__dirname, 'src', 'App.js'),
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    mode: 'development',
+
+    module: {
+        rules :[
+            {
+                test: /\.(le|c)ss$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test:/\.(jsx|js)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+        ]
+    }
+};
