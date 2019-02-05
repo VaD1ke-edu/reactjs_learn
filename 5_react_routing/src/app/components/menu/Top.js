b1568970e094402f6270a57b1c3d64921a2ccc9b
VaD1ke-edu <vladislav.slesarenko@gmail.com>
2019-02-05 23:10:36 +0300

    Activate topnav menu item on click

--- 5_react_routing/src/app/components/menu/Top.js
+++ 5_react_routing/src/app/components/menu/Top.js
@@ -5,13 +5,23 @@ import '../../styles/nav.less';
 
 class Top extends React.Component {
     render() {
+        const links = [
+            {link: '/', title: 'Главная'},
+            {link: '/posts', title: 'Посты'},
+            {link: '/comments', title: 'Комментарии'},
+            {link: '/users', title: 'Пользователи'},
+            {link: '/about', title: 'О нас'},
+        ];
+
+        const linkElems = links.map((item, index) => {
+            let className = 'topmenu__item';
+            className += window.location.pathname === item.link ? ' active' : '';
+            return <Link className={className} to={item.link} key={index}>{item.title}</Link>
+        });
+
         return (<div className="topmenu">
             <div className="topmenu__item topmenu__item_logo">React blog</div>
-            <Link className="topmenu__item" to="/">Главная</Link>
-            <Link className="topmenu__item" to="/posts">Посты</Link>
-            <Link className="topmenu__item" to="/comments">Комментарии</Link>
-            <Link className="topmenu__item" to="/users">Пользователи</Link>
-            <Link className="topmenu__item" to="/about">О нас</Link>
+            {linkElems}
         </div>);
     }
 }
