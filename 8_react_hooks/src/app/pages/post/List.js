22514ef4303b56a74e12072af379b45253d59c79
VaD1ke-edu <vladislav.slesarenko@gmail.com>
2019-02-14 18:06:45 +0300

    Use hooks

---
+++ 8_react_hooks/src/app/pages/post/List.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+
+import PostProvider from '../../models/post/Provider';
+import PostItem from '../../components/post/Item';
+
+const List = props => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        PostProvider.getList().then((data) => {
+            setPosts(data);
+        });
+    }, []);
+
+    const addPost = () => {
+        const maxId = Math.max.apply(Math, posts.map(o => o.id));
+        const newPost = {id: maxId + 1, title: 'title', body: 'body'};
+        setPosts([newPost, ...posts]);
+    };
+
+    const postItems = posts.map((item, index) => {
+        const id = item.id || index;
+        const link = '/posts/' + id;
+        return <PostItem title={item.title} text={item.body} link={link} key={id} />;
+    });
+
+    return (<div>
+            <h1 className="title">Посты</h1>
+            <button onClick={addPost}>Добавить пост</button>
+            <div className="list">
+                {postItems}
+            </div>
+        </div>);
+};
+
+export default List;
---
+++ 8_react_hooks/webpack.config.js
@@ -0,0 +1,43 @@
+//подключаем модули
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, 'src', 'App.js'),
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+
+    mode: 'development',
+
+    module: {
+        rules :[
+            {
+                test: /\.(le|c)ss$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test:/\.(jsx|js)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+        ]
+    },
+
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        historyApiFallback: true
+    },
+
+    plugins: [
+        new HtmlPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html'
+        })
+    ]
+};
